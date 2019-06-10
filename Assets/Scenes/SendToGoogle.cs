8e4883eadf57a48231a60fe69da5ecf9e798c171
Hobie <hobie_95@live.nl>
2019-06-10 13:17:09 +0200

    UI en feedback

--- Assets/Scenes/SendToGoogle.cs
+++ Assets/Scenes/SendToGoogle.cs
@@ -2,44 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class SendToGoogle : MonoBehaviour
 {
     public GameObject username;
-    public GameObject email;
-    public GameObject phone;
+
     public GameObject drop;
+    public GameObject dropModel;
 
     private string Name;
-    private string Email;
-    private string Phone;
+
     private int Dropper;
-     
+    private int DropperModel;
+
     [SerializeField]
     private string BASE_URL = "https://docs.google.com/forms/d/e/1FAIpQLSfyVtiVtOOgpz8qxQqm0r--ulrj8u2B_tXldNNAQhvGmb3O0g/formResponse";
 
-    IEnumerator Post(string name, string email, string phone, int drop)
+    IEnumerator Post(string name, int drop, int dropModel)
     {
         WWWForm form = new WWWForm();
         form.AddField("entry.1755259562", name);
-        form.AddField("entry.1200349711", email);
-        form.AddField("entry.472336746", phone);
+        form.AddField("entry.756974309", dropModel);
         form.AddField("entry.558954491", drop);
         byte[] rawData = form.data;
         WWW www = new WWW(BASE_URL, rawData);
         yield return www;
-        
+
     }
 
     public void Send()
     {
-        Name = username.GetComponent<InputField>().text;
-        Email = email.GetComponent<InputField>().text;
-        Phone = phone.GetComponent<InputField>().text;
+        Name = username.GetComponent<TMP_InputField>().text;
+
         Dropper = drop.GetComponent<Dropdown>().value;
+        DropperModel = drop.GetComponent<Dropdown>().value;
 
-
-        StartCoroutine(Post(Name, Email, Phone, Dropper));
+        StartCoroutine(Post(Name, Dropper, DropperModel));
 
     }
 
